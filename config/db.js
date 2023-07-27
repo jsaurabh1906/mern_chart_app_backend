b489e6f58db38f3ace1578e53621df8470493647
jsaurabh1906 <jsaurabh1906@gmail.com>
2023-07-27 15:47:20 +0530

    update

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Conneted To Mongodb Databse `);
+    //console.log(`Conneted To Mongodb Databse `);
   } catch (error) {
-    console.log(`Error in Mongodb`);
+    //console.log(`Error in Mongodb`);
   }
 };
 
